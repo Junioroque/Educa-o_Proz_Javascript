1b0ce36520af8edaf1b637848e15ee2308e66eb5
Junioroque <jcjunioroque@gmail.com>
2023-11-16 19:33:47 -0300

    exercicio 3

---
+++ Exercicio_javascript_Templte_String/style.js
@@ -0,0 +1,11 @@
+function calcular(num1, num2){
+    console.log(
+       `
+            ${num1} + ${num2} = ${num1 + num2}
+            ${num1} - ${num2} = ${num1 - num2}
+            ${num1} x ${num2} = ${num1 * num2}
+            ${num1} / ${num2} = ${num1 / num2}
+        `)   
+}
+ 
+calcular(10,3)
