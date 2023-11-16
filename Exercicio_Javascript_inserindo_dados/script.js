cd800ef3b7229284640ab583c32c323bd2fb1aff
Junioroque <jcjunioroque@gmail.com>
2023-11-16 20:25:41 -0300

    exercicio inserindo imagem

---
+++ Exercicio_Javascript_inserindo_dados/script.js
@@ -0,0 +1,8 @@
+let titulo = document.getElementById("titulo").innerHTML = "<h1>Pizzas</h1>"
+
+document.getElementById("img").innerHTML = "<img src='./img/pizza.jpg'/>"
+
+document.getElementById("preco").innerText = `
+Pizza em Promoção
+
+valor: R$ 25,00`
