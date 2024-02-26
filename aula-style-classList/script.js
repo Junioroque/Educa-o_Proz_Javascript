c927c8b0bec7d4cbe41f603845cee095d8a3b9a4
Junioroque <jcjunioroque@gmail.com>
2024-02-26 14:12:29 -0300

    adicionando erro na senha

--- aula-style-classList/script.js
+++ aula-style-classList/script.js
@@ -1,9 +1,8 @@
 
 const h2 = document.querySelector("h2")
 
-h2.style.color = "green"
+h2.style.color = "black"
 h2.style.fontSize = "50px"
-console.log(h2)
 
 
 const button = document.querySelector("button")
@@ -12,18 +11,17 @@ button.style.color = "brack"
 button.style.fontFamily ="Verdana, Geneva, Tahoma, sans-serif"
 button.style.fontWeight = "bold"
 button.style.borderRadius = "20px"
-button.style.backgroundColor = "yellow"
+button.style.border = "0px solid"
+button.style.backgroundColor = "orange"
 button.style.width = "150px"
-button.style.alignItems = "center"
-
 //adicionar class
 
 const userNameInput = document.querySelector("#login-usuario")
-userNameInput.classList.add("input.correct")
+userNameInput.classList.add("correct")
 
 
 const NameInput = document.querySelector("#login-senha")
-NameInput.classList.add("input.correct")
+NameInput.classList.add("error")
 
 const errorMessagem = document.querySelector(".error-text-senha")
 errorMessagem.classList.add("visible")
