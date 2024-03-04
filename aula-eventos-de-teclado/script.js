9a76f36fe4e47040ca9add71b6b0f707e09c0937
Junioroque <jcjunioroque@gmail.com>
2024-03-04 11:05:36 -0300

    abrindo outra pagina digitando 1

--- aula-eventos-de-teclado/script.js
+++ aula-eventos-de-teclado/script.js
@@ -1,6 +1,25 @@
 const linkPerfil = document.getElementById("link-perfil");
 const navPerfil = document.getElementById("nav-perfil");
+const linkPerfilDado = document.getElementById("link-perfil-dados")
 
 linkPerfil.addEventListener("mouseover", ()=> {
   navPerfil.style.display = "block"
+});
+
+document.addEventListener("keyup", (e) => {
+  console.log(e.key);
+  console.log(e.code);
+ 
+  if(e.code == "Escape"){
+    navPerfil.style.display = "none"
+  }
+
+  if(navPerfil.style.display == "block") {
+    if(e.code == "Digit1" || e.code == "Numpad1") {
+      linkPerfilDado.click()
+    }
+  }else if(e.code == "Digit1" || e.code == "Numpad1") {
+    navPerfil.style.display = "block"
+  }
 })
+
